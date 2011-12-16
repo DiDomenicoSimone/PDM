87af3f03e90180cdad23cce2dbdc99390b521b8c
simone <sim90one@hotmail.it>
2011-12-17 00:14:40 +0100

    "es1"

---
+++ DueActivity/src/due/Activity/Second.java
@@ -0,0 +1,14 @@
+package due.Activity;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Second extends Activity
+{
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+	
+
+}
+
+}
