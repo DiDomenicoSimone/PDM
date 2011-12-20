5c68d89a8238d5aa75e380dd133716b7557cd155
simone <sim90one@hotmail.it>
2011-12-20 11:49:49 +0100

    risorseXML

--- RisorseXML/src/risorse/XML/RisorseXMLActivity.java
+++ RisorseXML/src/risorse/XML/RisorseXMLActivity.java
@@ -15,40 +15,32 @@ public class RisorseXMLActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        XmlResourceParser parser = getResources().getXml(R.xml.compilation);
+        try {
+        	int eventType = parser.getEventType();
+        	while (eventType != XmlResourceParser.END_DOCUMENT) {
+        		if (eventType == XmlResourceParser.START_TAG) {
+        			String tagName = parser.getName();
+        			if ("brano".equals(tagName)) {
+        				String id = parser.getAttributeValue(0);
+        				String str = "Brano: " + id;
+        				Log.d("XML PARSER", str);
+        			}
+        		}
+        		else if (eventType == XmlResourceParser.TEXT) {
+        			String elementValue = parser.getText();
+        			String str = elementValue;
+    				Log.d("XML PARSER", str);
+        		}
+        		eventType = parser.next();
+        	}
+        }
+        catch (XmlPullParserException e) {
+        	e.printStackTrace();
+        }
+        catch (IOException e) {
+        	e.printStackTrace();
+        }
     }
-    
-    XmlResourceParser parser =getResources().getXml(R.xml.compilation);{
-    try
-    {
-    	int eventType = parser.getEventType();
-    	while (eventType != XmlResourceParser.END_DOCUMENT)
-    	{
-    		if (eventType == XmlResourceParser.START_TAG)
-    		{
-    			String tagName=parser.getName();
-    			if ("brano".equals(tagName))
-    			{
-    				String id=parser.getAttributeValue(0);
-    				String str = "Brano:" + id;
-    				Log.d("XML PARSER",str);
-    			}
-    		}
-    		else if (eventType == XmlResourceParser.TEXT)
-    		{
-    			String elementValue = parser.getText();
-    			String str = elementValue;
-    			Log.d("XML PARSER",str);
-    		}
-    		eventType = parser.next();
-    	}
-    }
-    catch (XmlPullParserException e)
-    {
-    	e.printStackTrace();
-    }
-    catch (IOException e)
-    {
-    	e.printStackTrace();
-    }
-}
 }
