3df3e23beba5fa759b276fd01b6ba72fdaf58a5e
simone <sim90one@hotmail.it>
2012-02-17 19:43:27 +0100

    esercitazione 3

--- ImageViewer/src/image/Viewer/ImageViewerActivity.java
+++ ImageViewer/src/image/Viewer/ImageViewerActivity.java
@@ -12,7 +12,10 @@ public class ImageViewerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        ImageView image = (ImageView) findViewById(R.id.imageView1);
-        image.setImageURI((Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM));
+        ImageView image = (ImageView) findViewById(R.id.imageView1); 			//Si prende il riferimento all'imageview creata nel layout
+        image.setImageURI((Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM)); //Con questa istruzione, quello che si fa è settare come imageview nel layout, l'eventuale imageview
+        																		   //trasportata da un intent lanciato dal sistema,ovvero si fa in modo che questa applicazione sia in grado
+        																		   //di gestire la visualizzazione delle immagine, cioè di ricevere intent con l'azione "SEND" che va registrata
+        																		   //nel file manifest,nel tag intent-filters
     }
 }
