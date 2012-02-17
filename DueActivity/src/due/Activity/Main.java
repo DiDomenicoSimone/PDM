bddd8a9771531c22f6a83d7d37e525533b3398c7
simone <sim90one@hotmail.it>
2012-02-17 16:53:50 +0100

    esercitazione 1

--- DueActivity/src/due/Activity/Main.java
+++ DueActivity/src/due/Activity/Main.java
@@ -9,17 +9,17 @@ import android.widget.Button;
 
 public class Main extends Activity {
     /** Called when the activity is first created. */
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        Button startButton = (Button) findViewById (R.id.startButton);
-        startButton.setOnClickListener (new OnClickListener()
-        {
-        	@Override
-        	public void onClick (View v)
+        super.onCreate(savedInstanceState);								//Viene invocato il costruttore della superclasse
+        setContentView(R.layout.main);									//Viene settato il content della schermata con la View passata come parametro, da una risorsa esterna
+        Button startButton = (Button) findViewById (R.id.startButton);	//Viene creato un oggetto di classe Button, e si prende il riferimento con l'id relativo 
+        startButton.setOnClickListener (new OnClickListener()			//Si aggancia al bottone un evento di click, e lo si gestisce tramite override del metodo "onClicK"
+        {	
+        	public void onClick(View v)
         	{
-        		startActivity(new Intent(Main.this, Second.class));
+        		startActivity(new Intent(Main.this, Second.class));		//Tramite il metodo "startActivity" viene lanciata una nuova activity, ovvero "Second.java",e ciò
+        																//viene realizzato tramite l'intent.
         	}
         });
     }
--- DueActivity/src/due/Activity/Second.java
+++ DueActivity/src/due/Activity/Second.java
@@ -6,9 +6,7 @@ public class Second extends Activity
 {
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second);
-	
-
-}
-
+        setContentView(R.layout.second); //In questa seconda activity, si associa un layout radice differente,ovvero quello con
+        								 //rifermento all'id R.layout.second, che si trova nella cartella res/layout
+	}			
 }
