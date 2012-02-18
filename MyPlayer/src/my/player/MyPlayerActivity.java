dc1f2b90723f923aa63269a106644be45d18ea3e
simone <sim90one@hotmail.it>
2012-02-18 18:02:24 +0100

    esercitazione 4

--- MyPlayer/src/my/player/MyPlayerActivity.java
+++ MyPlayer/src/my/player/MyPlayerActivity.java
@@ -10,26 +10,27 @@ import android.widget.Button;
 
 public class MyPlayerActivity extends Activity {
     /** Called when the activity is first created. */
-	MediaPlayer mp;
+	MediaPlayer mp;			//Variabile d'istanza di classe MediaPlayer
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        mp=MediaPlayer.create(MyPlayerActivity.this, R.raw.dst);
-        Button startButton = (Button) findViewById (R.id.button1);
-        startButton.setOnClickListener (new OnClickListener()
+        setContentView(R.layout.main); 
+        mp=MediaPlayer.create(MyPlayerActivity.this, R.raw.dst); 	//Viene creato il media player tramite il metodo "create" e passandogli come riferimento  il brano da riprodurre,
+        															//contenuto nella cartella res/raw;essendo una risorsa esterna viene preso il riferimento tramite l'id della classe R
+        Button startButton = (Button) findViewById (R.id.button1);	//Viene preso il riferimento al bottone creato nel layout
+        startButton.setOnClickListener (new OnClickListener()		//Si aggancia un evento di click al bottone
         {
         	public void onClick(View v)
         	{
-        		mp.start();
+        		mp.start();		//Quando viene cliccato il bottone "startButton" viene avviata l'esecuzione del media player
         	}
         });
         Button stopButton = (Button) findViewById (R.id.button2);
         stopButton.setOnClickListener (new OnClickListener()
         {
-        	public void onClick(View v)
+        	public void onClick(View v)	
         	{
-        		mp.pause();
+        		mp.pause(); 	//Quando viene cliccato il bottone "stopButton" viene interrotta l'esecuzione del media player
         	}
         });
         
@@ -39,6 +40,7 @@ public class MyPlayerActivity extends Activity {
     @Override
     public void onDestroy()
     {
-    	mp.release();
+    	mp.release();	//Si riscrive il metodo "onDestroy" per chiudere il media player,altrimenti alla chiusura dell'app tramite
+    					//tasto "back", il media player rimarrebbe attivo e quindi continuerebbe l'esecuzione della traccia audio
     }
 }
