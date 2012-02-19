860c1dcadaaa68633f7046c928b3efce22579051
simone <sim90one@hotmail.it>
2012-02-19 14:23:12 +0100

    esercitazione 5

--- DragView/src/drag/view/DragViewActivity.java
+++ DragView/src/drag/view/DragViewActivity.java
@@ -23,12 +23,14 @@ public class DragViewActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
  
-		ImageView iv = (ImageView) findViewById(R.id.imageView1);
-		iv.setOnTouchListener(new View.OnTouchListener() {
+		ImageView iv = (ImageView) findViewById(R.id.imageView1); //Si prende il riferimento all'immagine con id "imageView1"
+		iv.setOnTouchListener(new View.OnTouchListener() {		  //Si aggiunge all'imageview un listener per intercettare l'evento di touch
 
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				if(event.getActionMasked()==MotionEvent.ACTION_DOWN){
+			public boolean onTouch(View v, MotionEvent event) //Si sovrascrive il metodo "onTouch" per gestire tale evento sull'imageview
+			{
+				if(event.getActionMasked()==MotionEvent.ACTION_DOWN)	//Se l'azione sull'immagine è stata quella del tocco, cioè "ACTION_DOWN"
+				{														//allora vengono memorizzate le coordinate del touch e viene selezionata la view
 					offset_x = (int) event.getX();
 					offset_y = (int) event.getY();
 					selected_item = v;
@@ -36,16 +38,20 @@ public class DragViewActivity extends Activity {
 				return false;
 			}
 		});
-		RelativeLayout vg = (RelativeLayout) findViewById(R.id.relativeLayout1);
-		vg.setOnTouchListener(new View.OnTouchListener() {
+		
+		RelativeLayout vg = (RelativeLayout) findViewById(R.id.relativeLayout1);	//Si prende il riferimento al relative layout creato in main.xml
+		vg.setOnTouchListener(new View.OnTouchListener() {					//Si aggiunge al layout un listener per intercettare e poi gestire l'evento di touch
 
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				if(event.getActionMasked()==MotionEvent.ACTION_MOVE){
-					if(selected_item==null) return false;
-					int x = (int) event.getX() - offset_x;
-					int y = (int) event.getY() - offset_y;
+				if(event.getActionMasked()==MotionEvent.ACTION_MOVE)	//Controlla se l'evento associato al touch, è "ACTION_MOVE" cioè un touch persistente
+				{
+					if(selected_item==null) return false;      //Se non è selezionata nessuna view alla il return false, indica che l'evento non è stato consumato
+					int x = (int) event.getX() - offset_x;		//Si calcola la coordinata x relativa allo spostamento
+					int y = (int) event.getY() - offset_y;		//Si calcola la coordinata y relativa alla spostemanto
 
+					//Con questi due strutture condizionali si evita che le immagini non escano dalla schermata
+					
 					int w = getWindowManager().getDefaultDisplay().getWidth() -150;
 					int h = getWindowManager().getDefaultDisplay().getHeight()-150;
 					if (x > w)
@@ -56,23 +62,26 @@ public class DragViewActivity extends Activity {
 							new ViewGroup.MarginLayoutParams(
 									RelativeLayout.LayoutParams.WRAP_CONTENT,
 									RelativeLayout.LayoutParams.WRAP_CONTENT));
-					lp.setMargins(x, y, 0, 0);
+					lp.setMargins(x, y, 0, 0); 		//Margini del relative layout in funzione delle coordinate x e y
 
-					selected_item.setLayoutParams(lp);
+					selected_item.setLayoutParams(lp); //Si impostano i margini del layout alla view che è stata selezionata
 				}
-				if(event.getActionMasked()==MotionEvent.ACTION_UP){
-					selected_item=null;
+				
+				if(event.getActionMasked()==MotionEvent.ACTION_UP)
+				{
+					selected_item=null;	//Se il touch viene rilasciato, allora la view viene deselezionata
 				}
-				return true;
+				return true; //Indica che gli eventi di ACTION_UP e ACTION_DOWN sono stati consumati
 			}
 		});
 		
-		ImageView iv2 = (ImageView) findViewById(R.id.imageView2);
-		iv2.setOnTouchListener(new OnTouchListener() {
+		ImageView iv2 = (ImageView) findViewById(R.id.imageView2);  //Si prende il riferimento all'immagine con id "imageView2"
+		iv2.setOnTouchListener(new OnTouchListener() {				//Si aggiunge all'imageview un listener per intercettare l'evento di touch
 			
-			
-			public boolean onTouch(View v, MotionEvent event) {
-				if(event.getActionMasked()==MotionEvent.ACTION_DOWN){
+			public boolean onTouch(View v, MotionEvent event)		//Si sovrascrive il metodo "onTouch" per gestire tale evento sull'imageview 
+			{			
+				if(event.getActionMasked()==MotionEvent.ACTION_DOWN)	//Se l'azione sull'immagine è stata quella del tocco, cioè "ACTION_DOWN"
+				{														//allora vengono memorizzate le coordinate del touch e viene seleziona la view 
 					offset_x = (int) event.getX();
 					offset_y = (int) event.getY();
 					selected_item = v;
