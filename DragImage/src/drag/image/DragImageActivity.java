1b5416b3ec327e96ff44d2d2dd57060995afab0a
simone <sim90one@hotmail.it>
2012-02-19 13:09:59 +0100

    esercitazione 5

--- DragImage/src/drag/image/DragImageActivity.java
+++ DragImage/src/drag/image/DragImageActivity.java
@@ -6,8 +6,9 @@ import android.os.Bundle;
 public class DragImageActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView (this));
+        setContentView(new MyView (this));	//Viene settata la "MyView" come la view root per il layout di questa activity
     }
 }
--- DragImage/src/drag/image/MyView.java
+++ DragImage/src/drag/image/MyView.java
@@ -8,55 +8,58 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
-public class MyView extends View {
+public class MyView extends View {	//Si crea una classe estendendo la classe "View" ovvero quella alla quale appartengono tutte le componenti grafiche di un'app
 
-	public MyView(Context context) {
+	private int x=100;			//Coordinata x iniziale dell'immagine bitmap
+	private int y=100;			//Coordinata y iniziale dell'immagine bitmap
+	private Bitmap img=null;	//Si crea un oggetto "Bitmap"
+	private boolean dragOn=false; //Flag per la gestione degli eventi relativi all'immagine bitmap
+	
+	public MyView(Context context) //Si definisce il costruttore della classe "MyView",che permette di caricare nel bitmap "img"
+	{								//l'immagine presente nella cartella res/drawable con id "ic_launcher"
 		super(context);
 		BitmapFactory.Options opts = new BitmapFactory.Options();
 		opts.inJustDecodeBounds = true;
 		img = BitmapFactory.decodeResource(context.getResources(),R.drawable.ic_launcher);
 	}
 	
-	private int x=100;
-	private int y=100;
-	private Bitmap img=null;
-	private boolean dragOn=false;
-	
 	@Override
 	protected void onDraw(Canvas canvas)
-	{
-		canvas.drawBitmap(img,x,y,null);
-	}
+	{										//Il metodo onDraw è necessario per disegnare il bitmap sullo schermo,e ciò viene fatto
+		canvas.drawBitmap(img,x,y,null);	//attraverso un oggetto di classe Canvas, sul quale si invoca il metodo drawBitmap, al quale
+	}										//si passa il bitmap da disegnare e le coordinate x e y
 	
 	@Override
-	public boolean onTouchEvent (MotionEvent event)
-	{
-		int eventaction = event.getAction();
-		int tx = (int) event.getX();
-		int ty = (int) event.getY();
+	public boolean onTouchEvent (MotionEvent event)	//Si sovrascrive e implementa il metodo "onTouch" per intercettare la gestione degli 
+	{												//degli eventi di touch sullo schermo,altrimenti non si ha il controllo
+		int eventaction = event.getAction();		//Dato che l'oggetto "event" descrive il touch anche in termini di coordinate degli assi allora è possibile
+		int tx = (int) event.getX();				//estrarre tramite i metodi getter la coordinata x e y del punto in cui è avvenuto il touch
+		int ty = (int) event.getY();  
 		switch (eventaction)
 		{
-			case MotionEvent.ACTION_DOWN: 
-				if (tx>x && tx> x+img.getWidth() & ty>y & ty> y + img.getHeight());
-				{
+			case MotionEvent.ACTION_DOWN: 												//Controlla se l'azione coincide con ACTION_DOWN, cioè con la pressione sul bitmap
+				if (tx>x && tx> x+img.getWidth() & ty>y & ty> y + img.getHeight());		//Se il touch di coordinate tx e ty, è stato fa tto sul bitmap, allora si setta a true 
+				{																		//il flag "dragOn" e si visualizza un Toast che indica che il bitmap è stato cliccato 	
 					dragOn=true;
-					Toast.makeText(getContext(), "L'ImageView è stata cliccata ", Toast.LENGTH_LONG).show();
+					Toast.makeText(getContext(), "L'Immagine è stata cliccata ", Toast.LENGTH_LONG).show();
 				}
 				break;
 			
-			case MotionEvent.ACTION_MOVE:
-				x=tx;
-				y=ty;
-				invalidate();
+			case MotionEvent.ACTION_MOVE: //Controlla se l'azione è stata quella di "move" ovvero il touch persistente
+				if(dragOn)				  //Se "dragOn" è true, ovvero l'immagine è stata già "toccata" allora si 
+				{						  //aggiornano le coordinate che individuano la posizione del bitmap, con quelle
+					x=tx;				  //touch, e tramite il metodo statico "invalidate()" si richiama onDraw e quindi 
+					y=ty;				  //si ridisegna il bitmap con le nuove coordinate (l'effetto è quello dello spostamento)
+					invalidate();
+				}
 				break;
-			case MotionEvent.ACTION_UP:
-				dragOn=false;
-				Toast.makeText(getContext(), "L'ImageView è stata rilasciata ", Toast.LENGTH_LONG).show();
+			
+			case MotionEvent.ACTION_UP:	 //Controlla se l'azione è stata quella di rilasciare il "touch"
+				dragOn=false;			 //Si imposta a false il flag 
+				Toast.makeText(getContext(), "L'Immagine è stata rilasciata ", Toast.LENGTH_LONG).show(); //Si visualizza un Toast che sottolinea il rilascio dell'immagine
 				break;
 		}
-		return true;
+		return true;	//Uno degli eventi si è consumato e se ne può gestire un altro
 	}
 	
-	
-
 }
